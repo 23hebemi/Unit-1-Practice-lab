c3cc590fa3c471952aff03d2efff8d03e60eda5d
23hebemi <59451273+23hebemi@users.noreply.github.com>
2020-01-18 19:45:48 -0500

    Add files via upload

---
+++ hebert_unit1_practice.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# PART A
+print("Welcome to Information Technology")
+print("")
+print(" _       ___________\n| |     / /_  __/   |\n| | /| / / / / / /| |\n| |/ |/ / / / / ___ |\n|__/|__/ /_/ /_/  |_|")
+
+
+#PART B
+print("A lightning flash:\nbetween the forest trees\nI have seen water.\n\t-Shiki")
+
+print("**********\n*        *\n* MR. G  *\n* RM 119 *\n*        *\n**********")
+
+print("'Computer Science is no more about computers than astronomy is about telescopes' -Edsger W. Dijkstra")
+
+#PART C
+user_name = input("What is your name? ")
+favorite_animal = input("What is your favorite animal? ")
+print("Hello", user_name, ". Your favorite animal is the", favorite_animal, ".")
+
+number = input("What is your favorite number? ")
+print(number * 3)
+
+firstNum = int(input("Enter a test score: "))
+secondNum = int(input("Enter another test score: "))
+thirdNum = int(input("Enter a third test score: "))
+sumResult = firstNum + secondNum + thirdNum
+quotientResult = sumResult / 3
+print(quotientResult)
+
+firstWord = input("Enter a word: ")
+secondWord = input("Enter another word: ")
+thirdWord = input("Enter a third word: ")
+print(thirdWord, secondWord, firstWord)
+
+length = int(input("Enter the Length: "))
+width = int(input("Enter the width: "))
+print("  ",length,"\n")
+print(" ______")
+print("|", "    ", "|")
+print("|", "    ", "|", width)
+print("|------|")
+print("\n\nArea =", length * width)
+
+#PART D
+word1 = input("Enter a word: ")
+word2 = input("Enter another word: ")
+print(word1, word2)
+
+firstName = input("What is your first name? ")
+lastName = input("What is your last name? ")
+print(lastName, firstName)
+
+num = input("Enter a number: ")
+print(num * 8)
+
+wholeNumber = int(input("Enter a whole number: "))
+print(wholeNumber*12)
+
+#PART E
+a = input("Enter an animal: ")
+s = input ("Enter a sound: ")
+e = "E-I-E-I-O"
+
+print ("Old Macdonald had a farm, " + e)
+print ("And on his farm he had a" + a + "," + e)
+print ("With a " + s + "-" + s + " here and a" + s + "-" + s + " there")
+print ("Here a "+ s+ " there a " +s)
+print ("Everywhere a"  + s + "-" + s )
+print ("Old Macdonald had a farm," + e)
